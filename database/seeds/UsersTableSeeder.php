8bd99da8c0d6e701ec8248fb49b35ba8d12e839b
Your Name <you@example.com>
2019-11-19 13:35:35 +0800

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(500)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = '渣渣辉';
+        $user->email = 'zzh@qq.com';
+        $user->save();
+    }
+}

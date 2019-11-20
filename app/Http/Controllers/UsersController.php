484a6a6c7fd1c44cf36c219f176294a13d523f6e
Your Name <you@example.com>
2019-11-20 10:54:21 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,7 +35,10 @@ class UsersController extends Controller
     // 个人信息展示页面
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+                         ->orderBy('created_at', 'desc')
+                         ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     // 获取用户提交表单进行注册
